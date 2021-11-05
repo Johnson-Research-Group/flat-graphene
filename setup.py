dfba1deb5a911f3774760412f3625b9126c391ec
GHBrown <gabriel.h.brown@gmail.com>
2021-11-05 13:27:43 -0500

    Work on pip install

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='flatgraphene',  
+    version='0.1',
+    scripts=['bin/flatgraphene'] ,
+    author="Gabriel Brown",
+    author_email="gabriel.h.brown@gmail.com",
+    description="Generate mono and multilayer graphene geometries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Johnson-Research-Group/flat-graphene",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords=[
+        "twisted",
+        "bilayer",
+        "graphene",
+        "graphite",
+        "scientific",
+        "engineering",
+        "molecular dynamics",
+        "atomistic",
+    ],
+    install_requires=[
+        "numpy",
+        "ase",
+    ],
+ )
