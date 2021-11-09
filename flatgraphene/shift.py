3c3e5f28cb196f9574c32e6c8bc1b8364a6b1f90
dpalmer-anl <danpalm1998@gmail.com>
2021-11-09 13:23:46 -0600

    revert shift.py

--- flatgraphene/shift.py
+++ flatgraphene/shift.py
@@ -283,42 +283,9 @@ def make_graphene(stacking,cell_type,n_1,n_2,lat_con,n_layer,sep,a_nn=None,sym='
         
 
 if (__name__=="__main__"):
-    import twist
-    import os
     #example to modify when working on module
     atoms=make_graphene(stacking=['A','B','C'],cell_type='hex',
                         n_layer=3,n_1=3,n_2=3,lat_con=0.0,a_nn=1.5,
                         sep=3.0,sym=['O','F','N'],mass=np.array([0,1,2]))
-    #ase.visualize.view(atoms)
-    npoints=2
-    guess_t_=np.linspace(10,30,npoints)
-    a=2.529
-    sep=3.35
-    a_nn=a/np.sqrt(3)
-    base_path="C:/Users/danpa/Documents/research/twisted-graphene-geometry-optimization/lammps_io/"
-    for t in guess_t_:
-        p_found, q_found, theta_comp = twist.find_p_q(t,a_tol=4)
-        test_sheet = twist.make_graphene(cell_type='hex',n_layer=2,
-                                    p=p_found,q=q_found,lat_con=0.0,a_nn=a_nn,sym=["B","Ti"],
-                                    mass=[12.01,12.02],sep=sep,h_vac=3)
-        
-        real_t=theta_comp
-        name=str(np.round(real_t,decimals=2)).split(".")
-        name="_".join(name)
-        folder=base_path+"io_t"+name
-        if not os.path.exists(folder):
-            os.mkdir(folder)
-        
-        
-        #write_lammps_data(folder+"/lammps_data"+name+".data",test_sheet)
-        ase.io.write(folder+"/lammps_data"+name+".data",test_sheet,format="lammps-data",atom_style="full")
-    
-    folder=base_path
-    atoms=make_graphene(stacking=['A','B'],cell_type='hex',n_layer=2,
-		        n_1=5,n_2=5,lat_con=0.0,a_nn=a_nn,sep=sep,sym=['B','Ti'],h_vac=3)
-    ase.io.write(folder+"/io_AB/AB.data",atoms,format="lammps-data",atom_style="full")
-    
-    
-    atoms=make_graphene(stacking=['A','A'],cell_type='hex',n_layer=2,
-		        n_1=5,n_2=5,lat_con=0.0,a_nn=a_nn,sep=sep,sym=['B','Ti'],h_vac=3)
-    ase.io.write(folder+"/io_AA/AA.data",atoms,format="lammps-data",atom_style="full")
+    ase.visualize.view(atoms)
+   
