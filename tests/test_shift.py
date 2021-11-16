16b81a1a2c346bbe605a0942cf6661f4f05a5b9d
GHBrown <gabriel.h.brown@gmail.com>
2021-11-16 11:32:04 -0600

    Updated tests to match new capabilities

--- tests/test_shift.py
+++ tests/test_shift.py
@@ -1,16 +1,27 @@
 
+import numpy as np
 import ase
 from ase.visualize import view
 import flatgraphene as fg
 
+#single unit cell (hex/triclinic) with distance test
+test_dist = 1.44
+atoms = fg.shift.make_graphene(stacking='A',cell_type='hex',n_1=1,n_2=1,
+                               lat_con=0.0,n_layer=1,sep=1.0,a_nn=test_dist)
+pos = atoms.get_positions()
+dist = np.linalg.norm(pos[0] - pos[1])
+if (not np.isclose(dist,test_dist)):
+    print('ERROR: single unit cell test failed')
+
 
 #standard hexagonal AB bilayer with simple inputs
-atoms=fg.shift.make_graphene(stacking=['A','B'],cell_type='hex',n_1=3,n_2=3,
-			     lat_con=0.0,n_layer=2,sep=1.0,a_nn=1.5)
+atoms = fg.shift.make_graphene(stacking=['A','B'],cell_type='hex',n_1=3,n_2=3,
+	    		       lat_con=0.0,n_layer=2,sep=1.0,a_nn=1.5)
 ase.visualize.view(atoms)
 
-#arbitrary 5 layer system with per-layer inputs
-atoms=fg.shift.make_graphene(stacking=['A','B','A','SP','SP'],cell_type='rect',n_1=3,n_2=3,
-                             lat_con=0.0,n_layer=5,sep=[1.0,1.5,1.5,3.0,1.0],a_nn=1.5,
-                             sym='C',mass=12,h_vac=6)
+#arbitrary 5 layer system with per-layer inputs, and using every feature
+atoms = fg.shift.make_graphene(stacking=['A','B','A','SP','SP'],cell_type='rect',n_1=3,n_2=3,
+                               lat_con=0.0,n_layer=5,sep=[1.0,1.5,1.5,3.0,1.0],a_nn=1.5,
+                               sym=['C','B','N','O','Kr'],mass=[12.01,11,10,9,8],
+                               mol_id=[1,2,3,4,5],h_vac=6)
 ase.visualize.view(atoms)
--- tests/test_twist.py
+++ tests/test_twist.py
@@ -3,6 +3,16 @@ import ase
 from ase.visualize import view
 import flatgraphene as fg
 
+#test number of atoms in a cell
+n_21_78 = 28 #number of atoms in hexagonal cell of 21.78 degree twist angle
+p_found, q_found, theta_comp = fg.twist.find_p_q(21.79)
+atoms = fg.twist.make_graphene(cell_type='hex',n_layer=2,
+                               p=p_found,q=q_found,lat_con=0.0,a_nn=1.5,
+                               sep=3.35,h_vac=3)
+n_fg = (atoms.get_positions()).shape[0] #number of atoms in returned structure
+if (n_fg != n_21_78):
+    print('ERROR: 21.78 degree hex cell has wrong number of atoms')
+
 #small bilayer system with simple inputs
 p_found, q_found, theta_comp = fg.twist.find_p_q(21.79)
 atoms = fg.twist.make_graphene(cell_type='hex',n_layer=2,
@@ -14,5 +24,6 @@ ase.visualize.view(atoms)
 p_found, q_found, theta_comp = fg.twist.find_p_q(9.43)
 atoms = fg.twist.make_graphene(cell_type='hex',n_layer=5,
                                p=p_found,q=q_found,lat_con=0.0,a_nn=1.5,
-                               sep=[1.0,1.5,1.5,3.0,1.0],mass=12.02,h_vac=3)
+                               sep=[1.0,1.5,1.5,3.0,1.0],sym=['C','Kr','C','Kr','C'],
+                               mass=[1,2,3,4,5],mol_id=[1,101,2,202,3],h_vac=3)
 ase.visualize.view(atoms)
